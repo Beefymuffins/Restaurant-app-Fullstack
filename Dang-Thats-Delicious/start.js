085d8992b68039b3be2db347768da0f4ffacf57b
Beefymuffins <98682659+Beefymuffins@users.noreply.github.com>
2022-09-06 00:43:35 -0400

    stores

--- Dang-Thats-Delicious/start.js
+++ Dang-Thats-Delicious/start.js
@@ -12,9 +12,12 @@ mongoose.connection.on('error', (err) => {
 
 // READY?! Let's go!
 
+// Import all of our models
+require('./models/Store');
 
 // Start our app!
 const app = require('./app');
+
 app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${server.address().port}`);
