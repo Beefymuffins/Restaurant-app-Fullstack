099262921edd534be5495402b0176af36974367d
Beefymuffins <98682659+Beefymuffins@users.noreply.github.com>
2022-09-26 17:19:14 -0400

    Pagination

--- Dang-Thats-Delicious/controllers/storeController.js
+++ Dang-Thats-Delicious/controllers/storeController.js
@@ -52,9 +52,22 @@ exports.createStore = async (req, res) => {
 };
 
 exports.getStores = async (req, res) => {
+  // Pagination params
+  const page = req.params.page || 1;
+  const limit = 6;
+  const skip = page * limit - limit;
+
   // 1. Query the database for a list of all stores
-  const stores = await Store.find();
-  res.render('stores', { title: 'Stores', stores });
+  const storesPromise = Store.find().skip(skip).limit(limit).sort({ created: 'desc' });
+  const countPromise = Store.count();
+  const [stores, count] = await Promise.all([storesPromise, countPromise]);
+  const pages = Math.ceil(count / limit);
+
+  if (!stores.length && skip) {
+    res.redirect(`/stores/page/${pages}`);
+    return;
+  }
+  res.render('stores', { title: 'Stores', stores, page, pages, count });
 };
 
 const confirmOwner = (store, user) => {
